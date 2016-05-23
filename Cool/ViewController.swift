5ba075a0473cf0a5e87f200a2b0a714e3c98f315
Cindy Pom <cindypom@cindys-macbook-pro.local>
2016-05-23 17:49:35 -0400

    Initial Commit

---
+++ Cool/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Cool
+//
+//  Created by Cindy Pom on 2016-05-23.
+//  Copyright © 2016 Wallis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
